e31d243252dc202e60cec8b6de332d1fef01762b
mcreinhard <mcreinhard@gmail.com>
2014-04-16 15:52:27 -0700

    Implement functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,38 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+  extend(instance, stackMethods);
+  return instance;
 };
-  
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
+
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this._size++;
+  var current = {
+    value: value,
+    below : this._top
+  };
+  this._top = current;
+};
+
+stackMethods.pop = function(){
+  if (this._size > 0) {
+    this._size--;
+    var result = this._top.value;
+    this._top = this._top.below;
+    return result;
+  }
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
+
+stackMethods._size = 0;
+stackMethods._top = null;
