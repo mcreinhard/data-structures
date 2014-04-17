4fba885632056c9f34df01dc3c5f724a5082e2bf
mcreinhard <mcreinhard@gmail.com>
2014-04-17 11:57:12 -0700

    Implemented set, graph, hash table

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,20 +1,48 @@
 var Graph = function(){
+  this._storage = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  if (Object.keys(this._storage).length === 1) {
+    toNode = Object.keys(this._storage)[0];
+  }
+  this._storage[newNode] = {};
+  if (toNode !== undefined) {
+    this.addEdge(newNode, toNode);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return this._storage[node] !== undefined;
 };
 
 Graph.prototype.removeNode = function(node){
+  for (var connectedNode in this._storage[node]) {
+    this.removeEdge(node, connectedNode);
+  }
+  delete this._storage[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  return this._storage[fromNode][toNode] ? true : false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+  this._storage[fromNode][toNode] = true;
+  this._storage[toNode][fromNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this._storage[fromNode][toNode];
+  delete this._storage[toNode][fromNode];
+  for (var i = 0; i < arguments.length; i++) {
+    if (isEmpty(this._storage[arguments[i]])) {
+      delete this._storage[arguments[i]];
+    }
+  }
 };
+
+var isEmpty = function(obj) {
+  return Object.keys(obj).length === 0;
+}
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,30 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, []);
+  }
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(i).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for(var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) return bucket[j][1];
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for(var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) bucket.splice(j, 1);
+  }
 };
-
